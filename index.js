53494c2ce773cd0fa4f958cbfbeda7d51aa77fbb
Ashna Shah-Grover <as19@williams.edu>
2019-08-25 21:56:30 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Write your code here!
+
+var main = document.querySelector("main#main");
+main.remove();
+
+var newHeader = document.createElement("h1");
+newHeader.id = "victory";
+
+newHeader.innerHTML = "Ashna is the champion"
+
+document.body.appendChild(newHeader);

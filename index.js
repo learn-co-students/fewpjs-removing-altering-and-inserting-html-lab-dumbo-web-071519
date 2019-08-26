23d237c38a4a3b84a05f1a7e2fcd0a97d7cd176e
github username <github email address>
2019-08-25 23:25:38 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code here!
+
+document.body.removeChild(main)
+
+const newHeader = document.createElement('h1');
+newHeader.id = "victory"
+newHeader.innerHTML = "Flatiron School is the champion!";
+header.appendChild(h1)
